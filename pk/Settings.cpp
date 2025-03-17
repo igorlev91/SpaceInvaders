932d806908e1e440e61a811d73ed48e56309710e
igorlev91 <igorlevdansky91@gmail.com>
2025-03-17 11:49:08 +0300

    Settings implementation
    - Settings Reader and Settings class to read Actor class configuration from files

---
+++ pk/Settings.cpp
@@ -0,0 +1,205 @@
+#include "Settings.h"
+
+#include <sstream>
+#include <glm/gtc/matrix_transform.hpp>
+
+Settings::Settings()
+	: Map()
+{
+}
+
+Settings::Settings(const SettingsMap& InMap)
+	: Map(InMap)
+{
+}
+
+void Settings::SetMap(const SettingsMap& InMap)
+{
+	Map = InMap;
+}
+
+bool Settings::Exists(const Key& InKey) const
+{
+	return Map.count(InKey) > 0;
+}
+
+bool Settings::Get(const Key& InKey, const int Default, int& OutValue) const
+{
+	if (!Exists(InKey))
+	{
+		OutValue = Default;
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	OutValue = std::stoi(TempValue);
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, const float Default, float& OutValue) const
+{
+	if (!Exists(InKey))
+	{
+		OutValue = Default;
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	OutValue = std::stof(TempValue);
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, const std::string& Default, Value& OutValue) const
+{
+	if (!Exists(InKey))
+	{
+		OutValue = Default;
+		return false;
+	}
+
+	OutValue = Map.at(InKey);
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, IntList& OutList) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	StringList TempList = Split(TempValue);
+
+	OutList.clear();
+	for (const Value& Element : TempList)
+	{
+		OutList.push_back(std::stoi(Element));
+	}
+
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, FloatList& OutList) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	StringList TempList = Split(TempValue);
+
+	OutList.clear();
+	for (const Value& Element : TempList)
+	{
+		OutList.push_back(std::stof(Element));
+	}
+
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, StringList& OutList) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	OutList.clear();
+	OutList = Split(TempValue);
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, Vec2& OutVec) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	const StringList Splitted = Split(TempValue);
+	if (Splitted.size() < 2)
+	{
+		return false;
+	}
+
+	const FloatList Floats = AsFloat(Splitted);
+	OutVec.x = Floats[0];
+	OutVec.y = Floats[1];
+
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, Vec3& OutVec) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	const StringList Splitted = Split(TempValue);
+	if (Splitted.size() < 3)
+	{
+		return false;
+	}
+
+	const FloatList Floats = AsFloat(Splitted);
+	OutVec.x = Floats[0];
+	OutVec.y = Floats[1];
+	OutVec.z = Floats[2];
+
+	return true;
+}
+
+bool Settings::Get(const Key& InKey, Vec4& OutVec) const
+{
+	if (!Exists(InKey))
+	{
+		return false;
+	}
+
+	const Value TempValue = Map.at(InKey);
+	const StringList Splitted = Split(TempValue);
+	if (Splitted.size() < 4)
+	{
+		return false;
+	}
+
+	const FloatList Floats = AsFloat(Splitted);
+	OutVec.x = Floats[0];
+	OutVec.y = Floats[1];
+	OutVec.z = Floats[2];
+	OutVec.w = Floats[3];
+
+	return true;
+}
+
+Settings::StringList Settings::Split(const Value& InValue)
+{
+	std::stringstream Stream(InValue);
+	StringList Result;
+
+	while (Stream.good())
+	{
+		Value Element;
+		std::getline(Stream, Element, ',');
+		Result.push_back(Element);
+	}
+
+	return Result;
+}
+
+Settings::FloatList Settings::AsFloat(const StringList& InList)
+{
+	FloatList OutList;
+	for (const std::string& Element : InList)
+	{
+		OutList.push_back(std::stof(Element));
+	}
+
+	return OutList;
+}
---
+++ pk/Settings.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include <map>
+#include <memory>
+#include <string>
+#include <vector>
+#include <glm/detail/type_vec.hpp>
+
+class Settings
+{
+public:
+	typedef std::shared_ptr<Settings> SharedPtr;
+	typedef std::shared_ptr<const Settings> SharedConstPtr;
+
+	typedef std::string Key;
+	typedef std::string Value;
+	typedef std::vector<int> IntList;
+	typedef std::vector<float> FloatList;
+	typedef std::vector<Value> StringList;
+	typedef glm::vec2 Vec2;
+	typedef glm::vec3 Vec3;
+	typedef glm::vec4 Vec4;
+	typedef std::map<Key, Value> SettingsMap;
+
+	Settings();
+	Settings(const SettingsMap& InMap);
+
+	void SetMap(const SettingsMap& InMap);
+
+	bool Exists(const Key& InKey) const;
+
+	bool Get(const Key& InKey, const int Default, int& OutValue) const;
+	bool Get(const Key& InKey, const float Default, float& OutValue) const;
+	bool Get(const Key& InKey, const std::string& Default, Value& OutValue) const;
+
+	bool Get(const Key& InKey, IntList& OutList) const;
+	bool Get(const Key& InKey, FloatList& OutList) const;
+	bool Get(const Key& InKey, StringList& OutList) const;
+
+	bool Get(const Key& InKey, Vec2& OutVec) const;
+	bool Get(const Key& InKey, Vec3& OutVec) const;
+	bool Get(const Key& InKey, Vec4& OutVec) const;
+
+private:
+	static StringList Split(const Value& InValue);
+	static FloatList AsFloat(const StringList& InList);
+
+	SettingsMap Map;
+};
